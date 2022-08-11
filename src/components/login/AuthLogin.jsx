9ed60e68797acc3b909954919f6831019aff2ecf
mikye912 <mikye912@gmail.com>
2022-08-11 17:48:02 +0900

    kmk_로그인 서버요청 경로 변경

--- src/components/login/AuthLogin.jsx
+++ src/components/login/AuthLogin.jsx
@@ -50,7 +50,7 @@ export default function AuthLogin() {
             }
         }
 
-        axios.post('/api/login_process', {
+        axios.post('/api/login/AuthLogin', {
             userId: inputRef.current[0].value,
             userPw: common.base64Enc(inputRef.current[1].value)
         })
