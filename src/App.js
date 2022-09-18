169c1a2dc716d597c863d9746dc7544e9daf0f8b
KMK <mikye912@gmail.com>
2022-09-19 00:21:00 +0900

    kmk_20220919

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import Login from 'Views/Login/Login';
 import Sub_main from 'Views/Main/Sub_main';
 import { Provider } from "react-redux";
 import store from "Common/Redux/store";
+import { MobileView, BrowserView } from 'react-device-detect';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={ 
+            <>
+            {/* <MobileView>
+              test
+            </MobileView> */}
+            {/* <BrowserView> */}
+              <Login /> 
+            {/* </BrowserView> */}
+            </>
+            } />
           <Route path="/main/*" element={<Sub_main />} />
           <Route path="/*" element={<Sub_main />} />
         </Routes>
