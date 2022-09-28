100fce4952ef2ff16233096f72e478b7134504e8
mikye912 <mikye912@gmail.com>
2022-09-28 15:13:46 +0900

    kmk_20220928

--- src/Views/Login/Mobile.jsx
+++ src/Views/Login/Mobile.jsx
@@ -1,11 +1,11 @@
 import 'Css/mobile.css'
 import React from 'react';
-//import mobilePreparingImg from 'assets/img/img_mobile_preparing.png';
 
 const Mobile = () => {
-    const handleLinkCopy = () => {
-        navigator.clipboard.writeText(document.location.href);
-        alert('링크가 복사되었습니다!');
+
+    const handleGotoPc = () => {
+        sessionStorage.setItem('device', 'PC')
+        window.location.href = "/";
     };
 
     return (
@@ -18,13 +18,11 @@ const Mobile = () => {
                 아쉽게도 모바일은 지원하지 않아요😥 <br />
                 PC환경에서 이용해주세요!
             </div>
-            <button className="gotopc_btn" onClick={handleLinkCopy}>
+            <button className="gotopc_btn" onClick={handleGotoPc}>
                 PC로 보기
             </button>
         </div>
     );
 };
-
-/** styled components */
 
 export default Mobile;
